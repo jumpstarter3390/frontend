e63d2aa16ba85a5810292956f937acc2cf6d8200
David Mesa <dmeza38@yahoo.com>
2022-11-01 15:14:26 -0700

    Used for the SecondGrade page

---
+++ src/Second.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Second = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">This will be the Second Grade Page</h1>
+
+
+    </div>
+
+    );
+  };
+export default Second;
