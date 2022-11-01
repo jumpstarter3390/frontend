c68d10a247a9ae28ecd63196f0b924da46ddcf39
David Mesa <dmeza38@yahoo.com>
2022-11-01 15:13:22 -0700

    Used for the FirstGrade page

---
+++ src/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const First = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">This will be the First Grade Page</h1>
+
+
+    </div>
+
+    );
+  };
+export default First;
