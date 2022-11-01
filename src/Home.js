1f472258a6380cadce2cddb293a4b38a4d002a6e
David Mesa <dmeza38@yahoo.com>
2022-11-01 15:17:33 -0700

    HomePage where they will choose their grade

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+import Image from './4th1.jpg';
+
+
+const Home = () => {
+    return (
+      <div>
+        <h1 className="text-5xl text-center">This is Homepage</h1>
+
+        <div className="layout">
+          <Link to="/first" className="FirstGrade"><img src={Image} /> </Link>
+          <Link to="/second" className="SecondGrade"><img src={Image} /> </Link>
+          <Link to="/third" className="ThirdGrade"><img src={Image} /> </Link>
+          <Link to="/fourth" className="FourthGrade"><img src={Image} /> </Link>
+          <Link to="/fifth" className="FifthGrade"><img src={Image} /> </Link>
+        </div>
+      </div>
+
+      );
+    };
+export default Home;
