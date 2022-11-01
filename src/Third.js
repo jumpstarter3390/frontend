9352fed0d3f6d67f27ccb39be8ae84112b3239a7
David Mesa <dmeza38@yahoo.com>
2022-11-01 15:15:14 -0700

    Used for the ThirdGrade page

---
+++ src/Third.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Third = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">This will be the Third Grade Page</h1>
+
+
+    </div>
+
+    );
+  };
+export default Third;
