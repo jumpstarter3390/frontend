a1f10a86c5ba2d7e3590352edfc201fd56580713
David Mesa <dmeza38@yahoo.com>
2022-11-01 15:12:05 -0700

    Used for the developer page

---
+++ src/DevelopmentTeam.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Developers = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center">Developers</h1>
+        </div>
+    );
+};
+export default Developers;
