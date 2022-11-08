ba21f21c1829de7c63d615f17af33e1f0c4d796f
David Mesa <dmeza38@yahoo.com>
2022-11-07 19:40:28 -0800

    updated flash card

--- src/flashcard.js
+++ src/flashcard.js
@@ -2,7 +2,7 @@ import {useState, useEffect, useRef} from 'react';
 import useSound from 'use-sound';
 import './flashcard.css';
 import Button from 'react-bootstrap/Button';
-
+import SFX from './correct.mp3';
 const Flashcard = () => {
 
 	const [num1, setNum1] = useState(2);
@@ -12,7 +12,7 @@ const Flashcard = () => {
 	const [score, setScore] = useState(0);
 	const [correct, setCorrect] = useState(true);
 	const inputRef = useRef(null);
-	
+
 
 	const [playActive] = useSound(
 		'./correct.mp3',
@@ -24,7 +24,7 @@ const Flashcard = () => {
     { volume: 0.25 }
  	);
 
-	useEffect(() => {},[ans]) 
+	useEffect(() => {},[ans])
 
 
 	  function randomNumberInRange(min, max) {
@@ -33,7 +33,7 @@ const Flashcard = () => {
 
   	const handleRan = () => {
   	setNum1(randomNumberInRange(0, 10));
-  	
+
   	setNum2(randomNumberInRange(0, 10));
   	setCorrect(false);
 
@@ -42,50 +42,45 @@ const Flashcard = () => {
    const handleAns = () => {
 
    	setCorrect(true);
- 
+
 
    }
 	return (
 
+		<div className="c">
+		<div className="card">
 
-		<div className="center">
-		<div className="card">	
-	
-			
-			<h2>{num1}</h2>
-			<h3> X </h3>
-			<h2>{num2}</h2>
+			<h2 id= "num1">{num1}</h2>
+			<h2 id = "num2">{num2}</h2>
 
-			<input type="text" ref={inputRef} id="answer" name="answer" onChange={e => {setUsans(e.target.value); }}/>
-			<Button  onClick={e => 
-				{setCorrect(false)
+			<input type="text" ref={inputRef} id="answer" name="answer" onChange={e => {setUsans(e.target.value);} }/>
+			<Button onClick={e =>
+				{
+					setCorrect(false)
 				 setAns(num1 * num2)
 				 if(num1 * num2 == usans){
 				 	setScore(score + 1);
 				 	setNum1(randomNumberInRange(0, 10));
   					setNum2(randomNumberInRange(0, 10));
-  					playActive()
-  					playOn()
-  					inputRef.current.value = "";
+  						inputRef.current.value = "";
+							playOn(e);
 				 }
 				 else {
 				 	setScore(0);
 				 	setCorrect(true)
 				 	inputRef.current.value = "";
 				 }
-				 
+
 			}}> Submit answer </Button>
 
-			<h2>Score: {score}</h2>
-			
+			<h2 id = "score">Score: {score}</h2>
+
 			{usans == ans && <h2 style={{visibility: correct ? 'hidden' : 'visible'}}> Correct! </h2>}
-			{usans != ans && <h2 style={{visibility: correct ? 'visible' : 'hidden' }}> Incorrect Please Try Again! </h2>}
-			
-		</div>
-		</div>
+			{usans != ans && <h2 style={{visibility: correct ? 'visible' : 'hidden' }}> Incorrect </h2>}
 
+		</div>
+		</div>
 	);
-
 };
 
 export default Flashcard;
