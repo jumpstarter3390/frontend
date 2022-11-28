2ac075d9e64ac1373ca533d8be1208686db7259d
David Mesa <dmeza38@yahoo.com>
2022-11-27 23:04:32 -0800

    For third grade math

--- src/flashcard3.js
+++ src/flashcard3.js
@@ -1,90 +1,99 @@
+import React from 'react';
 import {useState, useEffect, useRef} from 'react';
 import useSound from 'use-sound';
-import './flashcard3.css';
+import './flashcard.css';
 import Button from 'react-bootstrap/Button';
 
-const Flashcard = () => {
+const Flashcard3 = () => {
 
 	const [num1, setNum1] = useState(2);
 	const [num2, setNum2] = useState(5);
+	const [ans, setAns] = useState(0);
 	const [usans, setUsans] = useState(0);
 	const [score, setScore] = useState(0);
 	const [correct, setCorrect] = useState(true);
 	const inputRef = useRef(null);
-	
+  const [playbackRate, setPlaybackRate] = React.useState(0.75);
+	const soundUrl = 'https://github.com/jumpstarter3390/frontend/blob/main/src/success.mp3?raw=true';
+	const wrong = 'https://github.com/jumpstarter3390/frontend/blob/main/src/incorrect.mp3?raw=true';
 
-	const [playActive] = useSound(
-		'./correct.mp3',
-		{volume: 0.25}
+	const [play] = useSound(soundUrl, {
+		playbackRate,
+		volume: 1,
+	});
 
-	);
-	const [playOn] = useSound(
-    './correct.mp3',
-    { volume: 0.25 }
- 	);
+	const handleClick = () => {
+     setPlaybackRate(playbackRate + 0.1);
+     play();
+   };
 
-	useEffect(() => {},[ans]) 
+	 const [play2] = useSound(wrong, {
+		 playbackRate,
+		 volume: 1,
+	 });
 
+	 const handleClick2 = () => {
+			 setPlaybackRate(playbackRate + 0.1);
+			 play2();
+		 };
+
+	useEffect(() => {},[ans])
 
 	  function randomNumberInRange(min, max) {
     	return Math.floor(Math.random() * (max - min + 1)) + min;
   	  }
 
   	const handleRan = () => {
-  	setNum1(randomNumberInRange(0,1000));
-  	
-  	setNum2(randomNumberInRange(0,1000));
+
+  	setNum1(randomNumberInRange(0, 10));
+  	setNum2(randomNumberInRange(0, 10));
   	setCorrect(false);
-
-
 	}
    const handleAns = () => {
 
    	setCorrect(true);
- 
 
-   }
+	}
+
 	return (
 
+		<div className="c">
+		<div className="card">
 
-		<div className="center">
-		<div className="card">	
-	
-			
-			<h2>{num1}</h2>
-			<h3> X </h3>
-			<h2>{num2}</h2>
+			<h2 id= "num1">{num1}</h2>
+			<h2 id = "num2">{num2}</h2>
 
-			<input type="text" ref={inputRef} id="answer" name="answer" onChange={e => {setUsans(e.target.value); }}/>
-			<Button  onClick={e => 
-				{setCorrect(false)
-				 setAns(num1 * num2)
-				 if(num1 * num2 == usans){
-				 	setScore(score + 1);
-				 	setNum1(randomNumberInRange(0, 1000));
-  					setNum2(randomNumberInRange(0, 1000));
-  					playActive()
-  					playOn()
-  					inputRef.current.value = "";
-				 }
-				 else {
-				 	setScore(0);
-				 	setCorrect(true)
-				 	inputRef.current.value = "";
-				 }
-				 
-			}}> Submit answer </Button>
+			<input type="text" ref={inputRef} id="answer" name="answer" onChange={e => {setUsans(e.target.value);} }/>
+			<Button onClick={() =>
+				{
+					setUsans()
+					setCorrect(false)
+				  setAns(num1 * num2)
 
-			<h2>Score: {score}</h2>
-			
-			{usans == ans && <h2 style={{visibility: correct ? 'hidden' : 'visible'}}> Correct! </h2>}
-			{usans != ans && <h2 style={{visibility: correct ? 'visible' : 'hidden' }}> Incorrect Please Try Again! </h2>}
-			
+				  	if(num1 * num2 == usans){
+					 		setScore(score + 1);
+				 			setNum1(randomNumberInRange(10, 50));
+  						setNum2(randomNumberInRange(10, 50));
+  						inputRef.current.value = "";
+							handleClick();
+
+				 		}
+				 		else {
+				 			setScore(0);
+				 			setCorrect(true)
+				 			inputRef.current.value = "";
+							handleClick2();
+				 		}
+					}}> Submit answer </Button>
+
+			<h2 id = "score">Score: {score}</h2>
+
+			{usans == ans && <h2 style={{visibility: correct ? 'hidden' : 'visible'}} > Correct! </h2>}
+			{usans != ans && <h2 style={{visibility: correct ? 'visible' : 'hidden' }}> Incorrect </h2>}
+
 		</div>
 		</div>
-
 	);
-
 };
 
-export default Flashcard;
+export default Flashcard3;
