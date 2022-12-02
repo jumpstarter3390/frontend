c20c4fe38e10be22b7870e6ddaca8390a1617e2f
David Mesa <dmeza38@yahoo.com>
2022-12-01 17:24:16 -0800

    styling for logout

--- src/Logout.js
+++ src/Logout.js
@@ -13,7 +13,7 @@ export default function Logout(){
     const email = window.localStorage.getItem('Email');
     const [usid, setUsid] = useState(0);
 
-    
+
     useEffect(() => {
         axios.get(`http://localhost:5000/register/${email}`)
         .then((response) => {
@@ -21,15 +21,16 @@ export default function Logout(){
         });
 
     }, []);
-    
+
     const putUsloggedout = () => {
 
     }
 
     return (
-        <div>
-            <h1> Currently logged in as {email}</h1>
-            <Button onClick={e => 
+        <div className= "logs">
+            <h1 id = "message"> Currently logged in as </h1>
+            <h1 id = "email"> {email} </h1>
+            <Button id="logButton" onClick={e =>
                 {        axios.put(`http://localhost:5000/register/update/${usid}`, {
             isLoggedIn: false,
         });
